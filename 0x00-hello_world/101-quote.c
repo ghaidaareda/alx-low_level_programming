3d8839e9f57ffe29ae9be8c505915cec46ffaf44
ghaidaareda <dodo.sa7afy@gmail.com>
2023-03-16 15:47:44 -0700

    quotes

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - exact print 
+ *
+ * Return: Always 1
+ */
+int main (void)
+{	
+	fwrite(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-1\n",50);
+	return (1);
+}
