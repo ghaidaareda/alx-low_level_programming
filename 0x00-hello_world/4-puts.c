9cea72387e722596ccb4307c01d87fcedd75a209
ghaidaareda <dodo.sa7afy@gmail.com>
2023-03-16 11:43:44 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,9 +4,8 @@
  * main - programme to print line with puts function
  * return - 0
  */
-
 int main(void)
 {
-puts(""Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle\n");
 return (0);
 }
