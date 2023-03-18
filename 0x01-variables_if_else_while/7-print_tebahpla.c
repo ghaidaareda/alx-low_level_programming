423afa01bd9687dcb5d3d2e5ea73c2fbbe35a485
ghaidaareda <dodo.sa7afy@gmail.com>
2023-03-18 06:12:09 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main- print numbers
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 'z'; n >= 'a'; n--)
+	putchar(n);
+	putchar('\n');
+	return (0);
+}
