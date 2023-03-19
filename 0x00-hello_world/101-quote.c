382ea2011bfa4f23a8ff282ca021c03c5ca0f4f5
ghaidaareda <dodo.sa7afy@gmail.com>
2023-03-19 16:05:19 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include <unistd.h>
 
 /**
- * main - exact print 
- *
- * Return: Always 1
- */
-int main(void)
-{	
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-1\n ,59");
+*main-exact print
+*
+*Return: Always 1
+*/
+	int main(void)
+{
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, 59);
 	return (1);
 }
