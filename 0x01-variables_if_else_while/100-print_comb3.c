641c52d1feda45fb03dae2e1d8fc95acae52756c
ghaidaareda <dodo.sa7afy@gmail.com>
2023-03-19 14:51:05 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+* main- print_comination
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x < 10; x++
+{
+	for (y = 0; y < 10; y++)
+{
+	if (x < y && x != y)
+{
+	putchar (48 + x);
+	putchar (48 + y);
+	putchar (',');
+	putchar (' ');
+}
+}
+}
+	putchar ('\n');
+	return (0);
+}
