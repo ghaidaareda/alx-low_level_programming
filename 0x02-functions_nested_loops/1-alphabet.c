91d519cb19994dd7916094ee68c49a89b7338fd4
ghaidaareda <dodo.sa7afy@gmail.com>
2023-03-21 15:16:26 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*print_alphabet-command
+*Return: Always void
+*/
+void print_alphabet(void)
+{
+	int n;
+
+	for (n = 'a'; n <= 'z'; n++)
+{
+	_putchar(n);
+}
+	_putchar('\n');
+}
