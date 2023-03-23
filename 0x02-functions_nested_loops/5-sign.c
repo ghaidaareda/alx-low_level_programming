a30c185471487e44fd1ccb64844dd537cda724b5
ghaidaareda <dodo.sa7afy@gmail.com>
2023-03-22 20:06:02 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,10 +2,11 @@
 
 /**
 * print_sign-sign
-* Return :1 in positive,0 in zero, -1 in negative
+* Return:1 in positive,0 in zero, -1 in negative
 * @n:number checked
 */
 int print_sign(int n)
+
 {
 	if (n > 48)
 {
@@ -14,8 +15,8 @@ int print_sign(int n)
 }
 	else if (n < 48)
 {
-        _putchar(45);
-	return (-1);  
+	_putchar(45);
+	return (-1);
 }
 	else if (n == 48)
 {
