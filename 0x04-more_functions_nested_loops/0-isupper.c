3d193c7c6cd591488f5b7273715c4a1deae6b3aa
ghaidaareda <dodo.sa7afy@gmail.com>
2023-03-23 05:48:31 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* _isupper-write upper case characters
+* Return: in upper case 1, in lower case 0
+* @c:upper case
+*/
+int _isupper(int c)
+{
+	if (c > 65 && c < 90)
+	return (1);
+	return (0);
+}
