f65ea91d3febcbd69436bd16729448451e2e87e0
ghaidaareda <dodo.sa7afy@gmail.com>
2023-03-24 11:48:48 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* more_numbers-write digits except
+* Return: void
+* void:digit
+*/
+void more_numbers(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a <= 10; a++)
+{
+	for (b = 0; b <= 14; b++)
+{
+	if (b > 9)
+{
+	_putchar ((b / 10) + 48);
+}
+	_putchar ((b % 10) + 48);
+}
+	_putchar ('\n');
+}
+}
