1a297d3c0146e2953a3b3bed5fbb7d3832c82931
ghaidaareda <dodo.sa7afy@gmail.com>
2023-03-24 13:07:34 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+*print_line-to print line
+*Return: void
+*@n:number of charater
+*/
+void print_line(int n)
+{
+	int chrlnt;
+
+	if (n == 0)
+{
+	_putchar ('\n');
+}
+	else
+{
+	for (chrlnt = 1; chrlnt <= n; chrlnt++)
+{
+	_putchar (45);
+}
+	_putchar ('\n');
+}
+}
