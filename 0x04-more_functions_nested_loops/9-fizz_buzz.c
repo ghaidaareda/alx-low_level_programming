80d4d305c9f5e78603b39b42801a9e9b7f9b3ec8
ghaidaareda <dodo.sa7afy@gmail.com>
2023-03-25 12:10:57 -0700

    9-fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,11 +11,11 @@
 
 	for (n = 1; n <= 100; n++)
 {
-	if (n % 3 == 0)
+	if ((n % 3 == 0) && !(n % 5 == 0))
 {
 	printf("Fizz ");
 }
-	else if (n % 5 == 0)
+	else if ((n % 5 == 0) && !(n % 3 == 0))
 {
 	printf("Buzz ");
 }
