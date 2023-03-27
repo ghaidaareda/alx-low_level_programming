eebdd074bd22bd4cedc6e47f0119d1a6a89429f2
ghaidaareda <dodo.sa7afy@gmail.com>
2023-03-27 14:23:35 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_rev-print string
+ * Return:void
+ * @s:string
+ */
+	void print_rev(char *s)
+{
+	while (*s != '\0')
+	{
+	_putchar(*s);
+	s--;
+	}
+	_putchar('\n');
+}
