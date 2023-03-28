5074b0295cbdfeafdf2a44ee6d8adeea0555aa72
ghaidaareda <dodo.sa7afy@gmail.com>
2023-03-28 08:06:45 -0700

    array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,9 +13,9 @@ void print_array(int *a, int n)
 		for (i = 0; i < n; i++)
 		{
 			printf("%d", a[i]);
-			if (i 1 != (n - 1))
+			if (i  != (n - 1))
 			{
-				pritnf(", ");
+				printf(", ");
 			}
 		}
 	printf("\n");
