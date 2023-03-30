f485a844e09c7554e6f74b8d3b92e543709b6b49
ghaidaareda <dodo.sa7afy@gmail.com>
2023-03-30 12:45:49 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * cap_string-change to upper case
+ * Return:char
+ * @n:string
+ */
+	char *cap_string(char *n)
+{
+	int i;
+
+	for (i = 0 ; n[i] != '\0'; i++)
+		for (i = 0; n[i] >= 'a' && n[i] <= 'z'; i++)
+		if (n[i - 1] == ' ' ||
+		n[i - 1] == '\t' ||
+		n[i - 1] == '\n' ||
+		n[i - 1] == ',' ||
+		n[i - 1] == ';' ||
+		n[i - 1] == '.' ||
+		n[i - 1] == '!' ||
+		n[i - 1] == '?' ||
+		n[i - 1] == '"' ||
+		n[i - 1] == '(' ||
+		n[i - 1] == ')' ||
+		n[i - 1] == '{' ||
+		n[i - 1] == '}' ||
+		i == 0)
+		{
+		n[i] = n[i] - 32;
+		}
+	return (n);
+}
