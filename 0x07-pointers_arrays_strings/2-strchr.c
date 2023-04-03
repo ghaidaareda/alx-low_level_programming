476fead4e79ce39a952d4cb282e7f87b3e851435
ghaidaareda <dodo.sa7afy@gmail.com>
2023-04-03 13:17:36 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strchr-locates a character in a string
+ * Return:c location or null
+ * @s:string pointer
+ * @c:character
+ */
+char *_strchr(char *s, char c)
+{
+
+	while (*s)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		else if (*s != c)
+			s++;
+	}
+	return ('\0');
+}
