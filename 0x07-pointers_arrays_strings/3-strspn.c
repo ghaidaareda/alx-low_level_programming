71b6c385886d0ecf6020211fa1ac32d7a1f49270
ghaidaareda <dodo.sa7afy@gmail.com>
2023-04-04 11:36:19 -0700

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,21 +7,20 @@
  */
 	unsigned int _strspn(char *s, char *accept)
 {
-	int i = 0;
+	unsigned int i;
 
-	int j = 0;
-
-	while (s[i] != '\0' && accept[j] != '\0')
-{
-	if (s[i] != accept[j])
+	unsigned int j;
+	
+	int len = 0;
+	for (i = 0; s[i] != '\0'; i++ )
 	{
-		return (0);
+		for (j = 0; accept[j +1] == '\0'; len++ )
+		{
+			if (s[i] != accept[j])
+			{
+			break;
+			}
 		}
-	else
-	{
-	i++;
-	j++;
 	}
-	}
-	return (j);
+	return (len);
 }
