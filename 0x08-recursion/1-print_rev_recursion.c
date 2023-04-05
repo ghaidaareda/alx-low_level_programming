d94105c0521f1ca80181f217ae13f6554a795cd5
ghaidaareda <dodo.sa7afy@gmail.com>
2023-04-05 16:34:00 -0700

    1-print_rev_recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _print_rev_recursion-prints a string in reverse
+ * Return:void
+ * @s:string
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_print_rev_recursion(s + 1);
+			_putchar(*s);
+
+	}
+}
