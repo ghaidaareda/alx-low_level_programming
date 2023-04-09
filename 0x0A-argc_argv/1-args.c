59c5aacda27de80c4d3a70d948fba47d05c36ed1
ghaidaareda <dodo.sa7afy@gmail.com>
2023-04-09 12:11:43 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main-print file name
+ * Return:0
+ * @argc:count
+ * @argv:vector
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
