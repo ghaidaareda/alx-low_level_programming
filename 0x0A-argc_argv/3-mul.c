5a4d30dff6156c773f2616989e120dc9a3b16c22
ghaidaareda <dodo.sa7afy@gmail.com>
2023-04-09 13:34:31 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ * main-multiply two numbers
+ * Return:0
+ * @argc:count
+ * @argv:vector
+ */
+	int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+
+		if (argc != 3)
+		{
+		printf("Error\n");
+			return (1);
+		}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
+	printf("%d\n", result);
+	return (0);
+}
