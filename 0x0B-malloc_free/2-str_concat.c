c2c9c85171b5e569747534ebf87ffa88776733d3
ghaidaareda <dodo.sa7afy@gmail.com>
2023-04-15 06:35:26 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,7 +9,7 @@
  */
 	char *str_concat(char *s1, char *s2)
 {
-	char *s3 = malloc((strlen(s1) + strlen(s2) + 1));
+	char *s3;
 
 	if (s1 == NULL)
 	{
@@ -19,11 +19,12 @@
 	{
 		s2 = "";
 	}
+	s3 = malloc((strlen(s1) + strlen(s2) + 1));
 		if (s3 == NULL)
 		{
 			return (NULL);
 		}
-	memcpy(s3, s1,sizeof(*s1));
+	strcpy(s3, s1);
 	strcat(s3, s2);
 	return (s3);
 }
