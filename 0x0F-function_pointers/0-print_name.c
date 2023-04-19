a64542e4a34e84e719fc0f317a3ea52197f6f27f
ghaidaareda <dodo.sa7afy@gmail.com>
2023-04-19 12:57:45 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ * print_name-function that prints aname 
+ * Return: void
+ * @name:the return name 
+ * @f: pointer to a function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
