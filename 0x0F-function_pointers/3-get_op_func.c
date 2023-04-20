ec8920306091ecb1ab6ecad883b77d57a0e0ee99
ghaidaareda <dodo.sa7afy@gmail.com>
2023-04-20 08:20:02 -0700

    calc

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -3,27 +3,27 @@
 /**
  * get_op_func-selects the correct function to perform the operation asked
  *@s:string of operators
- *@int:arguments
  *Return: A pointer to the corresponding function, or NULL if
  *the operator is not recognized.
  */
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
 	};
-    int i = 0;
-    while (ops[i].op != NULL)
-    {
-	    if(*(ops[i].op) == *s && *(s + 1) == '\0');
-		    return (ops[i].f);
-	    i++;
-    }
-    return (NULL);
+	int i = 0;
+
+	while (ops[i].op != NULL)
+	{
+	if (*(ops[i].op) == *s && *(s + 1) == '\0')
+	return (ops[i].f);
+	i++;
+	}
+	return (NULL);
 }
 
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,73 +9,29 @@
  */
 int main(int argc, char *argv[])
 {
-        int num1, num2, result;
+	int num1, num2, result;
 
-        int (*op_func)(int, int);
+	int (*op_func)(int, int);
 
-        if (argc != 4)
-        {
-        printf("Error\n");
-        return (1);
-        }
-        num1 = atoi(argv[1]);
-        num2 = atoi(argv[3]);
-        op_func = get_op_func(argv[2]);
-        if (op_func == NULL || argv[2][1] != '\0')
-        {
-        printf("Error\n");
-        return (2);
-        }
-        result = op_func(num1, num2);
-        printf("%d\n", result);
-        return (0);
-}
-/**
- * perform arithmetic operation on two integers
- *
- * @param num1 first integer
- * @param op operator string
- * @param num2 second integer
- * @return result of the arithmetic operation
- */
-int perform_operation(int num1, char *op, int num2)
-{
-int result;
-
-	if (*op == '+')
-	{
-	result = num1 + num2;
-	}
-	else if (*op == '-')
-	{
-	result = num1 - num2;
-	}
-	else if (*op == '*')
-	{
-	result = num1 * num2;
-	}
-	else if (*op == '/')
-	{
-	if (num2 == 0)
+	if (argc != 4)
 	{
 	printf("Error\n");
-	exit(100);
+	exit(98);
 	}
-	result = num1 / num2;
-	}
-	else if (*op == '%')
-	{
-	if (num2 == 0)
-	{
-	printf("Error\n");
-	exit(100);
-	}
-	result = num1 % num2;
-	}
-	else	
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op_func = get_op_func(argv[2]);
+	if (op_func == NULL || argv[2][1] != '\0')
 	{
 	printf("Error\n");
-	exit(99);
+	return (99);
 	}
-	return (result);
+	if (num2 == 0 && (argv[2][0] == '/' || argv[2][0] == '%'))
+	{
+	printf("Error\n");
+	return (100);
+	}
+	result = op_func(num1, num2);
+	printf("%d\n", result);
+	return (0);
 }
