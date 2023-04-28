1353cbed6b19b6e75bc0f0bf0b54e9028a885bd8
ghaidaareda <dodo.sa7afy@gmail.com>
2023-04-28 13:30:00 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -21,11 +21,17 @@ size_t print_list(const list_t *h)
 	ptr = h;
 	while (ptr != NULL)
 	{
+		if (ptr->str == NULL)
+		{
+			printf("[0](nil)\n");
+		}
+		else
+		{
 		printf("[%u] %s\n", ptr->len, ptr->str);
+		}
 		count++;
 		ptr = ptr->next;
+		
 	}
 	return (count);
 }
-
-
