1a12f2e0bbed5f07e6712a2db708b408e56f488b
ghaidaareda <dodo.sa7afy@gmail.com>
2023-05-09 08:47:55 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+/**
+ * create_file-function to create a file
+ * Return:1 (success), -1 (faliure)
+ * @filename:pointer to a file name
+ * @text_content:text to be written in file created
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	long int len = strlen(text_content);
+
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600)
+	if (filename == NULL || fd == -1 || fchmod(fd, 0600) == -1)
+		return (-1);
+	if (text_content == NULL)
+		if (write(fd, "", 0) != 0)
+		{
+			close(fd);
+			return (-1);
+		}
+	if (write(fd, text_content, len) != len)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
