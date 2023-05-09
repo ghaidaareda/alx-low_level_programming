56b4a7c3edc154f8a9b577a7452793565d8558cb
ghaidaareda <dodo.sa7afy@gmail.com>
2023-05-08 18:25:58 -0700

    update buffer

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,33 +13,37 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	char buffer[1024];
-	size_t bytes_read, bytes_written;
+	char *buffer;
+	ssize_t bytes_read, bytes_written;
 
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		return (0);
+	}
 	fd = open(filename, O_RDONLY);
-	if (filename == NULL)
+	if (filename == NULL || fd == -1)
 	{
+		free(buffer);
 		return (0);
 	}
-	if (fd == -1)
+	bytes_read = read(fd, buffer, letters);
+	if (bytes_read == -1)
 	{
+		free(buffer);
 		return (0);
 	}
-	bytes_read = read(fd, buffer, sizeof(buffer) < letters
-	? sizeof(buffer) : letters);
-	if (bytes_read == 0)
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written == -1)
 	{
-		return (0);
-	}
-	bytes_written = write(STDOUT_FILENO, buffer, bytes_read < letters
-	? bytes_read : letters);
-	if (bytes_written == 0)
-	{
+		free(buffer);
 		return (0);
 	}
 	if (close(fd) == -1)
 	{
+		free(buffer);
 		return (0);
 	}
+	free(buffer);
 	return (bytes_written);
 }
