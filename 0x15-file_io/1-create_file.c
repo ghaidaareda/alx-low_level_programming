afb1b06f61a2ec3286c919411e6a767b10a249d2
ghaidaareda <dodo.sa7afy@gmail.com>
2023-05-09 09:40:45 -0700

    updated condition

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,8 +23,7 @@ int create_file(const char *filename, char *text_content)
 			close(fd);
 			return (-1);
 		}
-	if (write(fd, text_content, len) != len ||
-	write(fd, text_content, len) == -1)
+	if (write(fd, text_content, strlen(text_content)) != len)
 	{
 		close(fd);
 		return (-1);
