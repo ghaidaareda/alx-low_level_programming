3ba417308b7ed61b84a0b4f35dff56dc174f7fdd
ghaidaareda <dodo.sa7afy@gmail.com>
2023-05-09 08:50:25 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	long int len = strlen(text_content);
 
-	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600)
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (filename == NULL || fd == -1 || fchmod(fd, 0600) == -1)
 		return (-1);
 	if (text_content == NULL)
