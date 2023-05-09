cadc0e3942df14d316c0a094ee6452894bd1839c
ghaidaareda <dodo.sa7afy@gmail.com>
2023-05-09 11:23:23 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL || fd == -1 || fchmod(fd, 0600) == -1)
 		return (-1);
 	if (text_content == NULL)
-		if (write(fd, "", 0) != 0)
+		if (write(fd, "\0", 0) != 0)
 		{
 			close(fd);
 			return (-1);
