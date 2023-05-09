3fddff512b8a54e323b54c40448fe5cae843af54
ghaidaareda <dodo.sa7afy@gmail.com>
2023-05-09 11:44:22 -0700

    betty

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,10 +24,11 @@ int create_file(const char *filename, char *text_content)
 			return (-1);
 		}
 	}
-	else 
+	else
 	{
 	long int len = strlen(text_content);
-	if (write(fd, text_content, len) != len)
+
+		if (write(fd, text_content, len) != len)
 		{
 			close(fd);
 			return (-1);
