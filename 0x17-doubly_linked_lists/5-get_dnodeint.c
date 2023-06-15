4d081a063979aa301958c0e05921d1e42c4b0366
ghaidaareda <dodo.sa7afy@gmail.com>
2023-06-15 16:02:26 -0700

    5-get_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -9,6 +9,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	dlistint_t *ptr = head;
 	unsigned int i;
+
 	for (i = 0; i < index && ptr != NULL; i++)
 	{
 		ptr = ptr->next;
