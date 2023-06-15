328835c11fe65a3a6e8250698412ecaa0c4a427c
ghaidaareda <dodo.sa7afy@gmail.com>
2023-06-15 15:57:54 -0700

    5-get_dnodeint.c

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ *get_dnodeint_at_index-function returns the nth node of a linked list
+ *Return:nth node of a linked list
+ *@head:pointer to head node
+ *@index:index of node wanted
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *ptr = head;
+	unsigned int i;
+
+	if (ptr != NULL)
+	{
+		for (i = 0; i < index; i++)
+		{
+			ptr = ptr->next;
+		}
+	return (ptr);
+	}
+	return (NULL);
+}
