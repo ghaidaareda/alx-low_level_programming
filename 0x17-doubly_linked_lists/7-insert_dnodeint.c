5accd793dfe025a890f3e420317ea8caecc18cf5
ghaidaareda <dodo.sa7afy@gmail.com>
2023-06-17 15:14:31 -0700

    add_at_index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,7 +9,7 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new_node, *temp;
-	unsigned int i;
+	unsigned int i = 0;
 	unsigned int count = 0;
 
 	if (idx == 0)
