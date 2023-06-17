b6ee0d5d38d001824f1623d12a6fbfa7ac0d7c67
ghaidaareda <dodo.sa7afy@gmail.com>
2023-06-17 15:07:50 -0700

    add_at_index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -43,8 +43,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			return (NULL);
 		}
 	}
-	new_node->next = temp;
 	new_node->prev = temp->prev;
+	new_node->next = temp;
 	temp->prev->next = new_node;
 	temp->prev = new_node;
 	return (new_node);
