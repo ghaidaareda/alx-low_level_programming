ea8d3d507e1a5e87ff727903341f2913e4708914
ghaidaareda <dodo.sa7afy@gmail.com>
2023-06-19 12:10:45 -0700

    100-operations.so

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+/**
+ * add-add function
+ * Return: a + b
+ * @a:a
+ * @b:b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub-subtract function
+ * Return: a - b
+ * @a:a
+ * @b:b
+ */
+int sub(int a, int b)
+{
+        return (a - b);
+}
+/**
+ * mul-multiply function
+ * Return: a * b
+ * @a:a
+ * @b:b
+ */
+int mul(int a, int b)
+{
+        return (a * b);
+}
+/**
+ * div-divide function
+ * Return: a / b
+ * @a:a
+ * @b:b
+ */
+int div(int a, int b)
+{
+        return (a / b);
+}  
+/**
+ * mod-modulus function
+ * Return: a % b
+ * @a:a
+ * @b:b
+ */
+int mod(int a, int b)
+{
+        return (a % b);
+}
