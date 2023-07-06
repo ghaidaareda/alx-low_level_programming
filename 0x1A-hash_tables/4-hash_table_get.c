5f7a51c940e669233fcf90b04674672258cbe268
ghaidaareda <dodo.sa7afy@gmail.com>
2023-07-06 16:52:35 -0700

    4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,7 +10,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *node;
 
-	if (key == NULL || ht == NULL)
+	if (key == NULL || ht == NULL || *key == '\0')
 	{
 		return (NULL);
 	}
