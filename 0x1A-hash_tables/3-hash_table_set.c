48821250ece8f4b032bb436971941635520415eb
ghaidaareda <dodo.sa7afy@gmail.com>
2023-07-07 11:24:46 -0700

    betty

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,12 +10,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 	hash_node_t *newnode;
-	
+
 	if (key == NULL || ht == NULL || ht->array == NULL)
 	{
 		return (0);
 	}
-	index = key_index((const unsigned char *)key , ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 	newnode = ht->array[index];
 	while (newnode != NULL)
 	{
