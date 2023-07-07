fda6f1140c1e9647ad81c845c0705309826894a3
ghaidaareda <dodo.sa7afy@gmail.com>
2023-07-07 11:22:02 -0700

    update_3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,13 +10,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 	hash_node_t *newnode;
-
-	if (key == NULL || ht == NULL || *key == '\0')
+	
+	if (key == NULL || ht == NULL || ht->array == NULL)
 	{
 		return (0);
 	}
-	index = hash_djb2((const unsigned char *)key) % ht->size;
-	for (newnode = ht->array[index]; newnode != NULL; newnode->next = ht->array[index])
+	index = key_index((const unsigned char *)key , ht->size);
+	newnode = ht->array[index];
+	while (newnode != NULL)
 	{
 		if (strcmp(newnode->key, key) == 0)
 		{
@@ -24,23 +25,17 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			newnode->value = strdup(value);
 			return (1);
 		}
+		newnode = newnode->next;
 	}
+	newnode = ht->array[index];
 	newnode = malloc(sizeof(hash_node_t));
 	if (newnode == NULL)
 	{
 		return (0);
 	}
-	newnode->key = malloc(strlen(key) + 1);
-	if (newnode->key == NULL)
-		return (0);
-	newnode->value = malloc(strlen(value) + 1);
-	if (newnode->value == NULL)
-		return (0);
 	newnode->key = strdup(key);
 	if (value == NULL)
-	{
 		newnode->value = NULL;
-	}
 	else
 	{
 	newnode->value = strdup(value);
