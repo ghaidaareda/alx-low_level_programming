28826286ded76858259ec0afcbe0d1fd826f64cf
ghaidaareda <dodo.sa7afy@gmail.com>
2023-10-11 01:16:24 +0300

    0-linear

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef _search_algos_h_
+#define _search_algos_h_
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
